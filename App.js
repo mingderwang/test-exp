ec916fafb8d78d90c30954b1e16a53b8ee2a774b
Ming-der Wang <ming@log4analytics.com>
2018-10-13 18:54:56 +0800

    expo-processing ok

--- App.js
+++ App.js
@@ -1,35 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Expo from 'expo';
-import ExpoPixi from 'expo-pixi';
-
-import './global';
-
-const Web3 = require('web3');
-const web3 = new Web3(
-  new Web3.providers.HttpProvider('https://mainnet.infura.io/'),);
-
+import {
+  ProcessingView
+} from 'expo-processing';
 
 export default class App extends React.Component {
-  render() {
-    web3.eth.getBlock('latest').then(console.log);
-    return (
- <View style={styles.container}>
-  <Expo.GLView
-    style={{ flex: 1 }}
-    onContextCreate={async context => {
-      const app = ExpoPixi.application({ context });
-      const sprite = await ExpoPixi.spriteAsync('http://i.imgur.com/uwrbErh.png');
-      app.stage.addChild(sprite);
-    }}
-  />
- </View>
-    );
-  }
-}
+    render() {
+      return ( <
+        ProcessingView style = {
+          {
+            flex: 1
+          }
+        }
+        sketch = {
+          this._sketch
+        }
+        />
+      );
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-});
+    _sketch = (p) => {
+        p.setup = () => {
+          p.strokeWeight(7);
+        }
+
+        const harom = (ax, ay, bx, by, level, ratio) => {
+          if (level <= 0) {
+            return;
+          }
+
+          const vx = bx - ax;
+          const vy = by - ay;
+          const nx = p.cos(p.PI / 3) * vx - p.sin(p.PI / 3) * vy;
+          const ny = p.sin(p.PI / 3) * vx + p.cos(p.PI / 3) * vy;
+          const cx = ax + nx;
+          const cy = ay + ny;
+          p.line(ax, ay, bx, by);
+          p.line(ax, ay, cx, cy);
+          p.line(cx, cy, bx, by);
+
+          harom(
+            ax * ratio + cx * (1 - ratio),
+            ay * ratio + cy * (1 - ratio),
+            ax * (1 - ratio) + bx * ratio,
+            ay * (1 - ratio) + by * ratio,
+            level - 1,
+            ratio);
+        }
+ p.draw = () => {
+   p.background(240);
+   harom(
+     p.width - 142, p.height - 142, 142, p.height - 142, 6,
+     (p.sin(0.0005 * Date.now() % (2 * p.PI)) + 1) / 2);
+ }
+}
+}
