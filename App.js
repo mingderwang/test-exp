0f8480db9338f9774bab35114a27ceb39b2f4afc
Ming-der Wang <ming@log4analytics.com>
2018-10-13 11:02:39 +0800

    ok for web3 getBlock()

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './global';
+
+const Web3 = require('web3');
+const web3 = new Web3(
+  new Web3.providers.HttpProvider('https://mainnet.infura.io/'),);
+
+
 export default class App extends React.Component {
   render() {
+    web3.eth.getBlock('latest').then(console.log);
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
