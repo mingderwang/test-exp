918ea3e43c468e6e7f40c8a79fe5670e3c834d6e
Ming-der Wang <mingderwang@gmail.com>
2018-10-23 15:22:12 +0800

    ok to use truffle hdwallet for account

--- App.js
+++ App.js
@@ -1,43 +1,49 @@
 import React from "react";
-import {
-  Text,
-  View
-} from "react-native";
+const Promise = require("bluebird");
+import { Text, View } from "react-native";
 import "./global";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      titleText: "waiting for data"
+      titleText: "waiting for data",
+      account: "creating..."
     };
   }
-  componentWillMount() {
-    const Web3 = require("web3");
-    web3 = new Web3(
-      new Web3.providers.HttpProvider("https://mainnet.infura.io/")
-    );
-    web3.eth.getBlockNumber().then(blockNumber => {
-      this.setState({
-        titleText: blockNumber
-      });
+  async componentWillMount() {
+    var HDWalletProvider = require("truffle-hdwallet-provider");
+    var mnemonic =
+      "manage velvet tide tube object spin identify neither foot together wagon tip";
+    var hd = new HDWalletProvider(mnemonic, "https://mainnet.infura.io/");
+    web3 = new Web3(hd);
+    Promise.promisifyAll(web3.eth);
+    let accounts = await web3.eth.getAccountsAsync();
+    this.setState({
+      account: accounts[0]
     });
-   // var HDWalletProvider = require("truffle-hdwallet-provider");
+    let blockNumber = await web3.eth.getBlockNumberAsync();
+    this.setState({
+      titleText: blockNumber
+    });
+    console.log(blockNumber)
   }
   render() {
-      return (
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#44bb44"
-          }}
-        >
-          <View>
-            <Text> BlockNumber: </Text>
-            <Text color="#ffffff">{this.state.titleText}</Text>
-          </View>
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#44bb44"
+        }}
+      >
+        <View>
+          <Text> BlockNumber: </Text>
+          <Text color="#ffffff">{this.state.titleText}</Text>
+          <Text> NewAccount: </Text>
+          <Text color="#ffffff">{this.state.account}</Text>
         </View>
-      );
+      </View>
+    );
   }
 }
